df298980d322f6565e4736487f9f0033a63dd3b7
EpicHigh <prasit_tongpradit@windowslive.com>
2018-08-03 19:19:10 +0700

    Add a lot of feature to Create page.

--- src/app/routes.js
+++ src/app/routes.js
@@ -4,16 +4,16 @@ import Index from "../pages/index/Index";
 import SignUp from "../pages/Signup";
 import SignIn from "../pages/Signin";
 import Create from "../pages/Create";
-import Error from "../pages/404"
+import Error from "../pages/404";
 
 const Routes = () => (
   <Switch>
     <Route path="/" exact component={Index} />
     <Route path="/signup" exact component={SignUp} />
-    <Route path="/signin" exact component={SignIn}/>
+    <Route path="/signin" exact component={SignIn} />
     <Route path="/create" exact component={Create} />
-	  <Route path='/404' component={Error} />
-	    <Redirect from='*' to='/404' />
+    <Route path="/404" component={Error} />
+    <Redirect from="*" to="/404" />
   </Switch>
 );
 
--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,83 +1,101 @@
 import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
-import api from "../db/server"
-import axios from "axios";
+import uuid from "uuid";
+import { storage, database } from "../db/firebase";
 
 class Create extends Component {
-  handleFormSubmit = async event => {
+  onFormSubmit = async event => {
     event.preventDefault();
     const caption = event.target.elements.caption.value;
     const photo = event.target.elements.photo.files[0];
-    const data = new FormData();
-    data.append("caption", caption);
-    data.append("photo", photo);
-    await this.postRequest(data);
-  };
-
-  postRequest = async data => {
     try {
-    	const options = {
-		    method: "post",
-		    url: `${api}/create`,
-		    data: data,
-		    headers: {"Content-Type": "multipart/form-data"}
-	    };
-      const response = await axios(options);
-      console.log(response.statusText);
-      this.setState({statusText: response.statusText})
+      if (caption && photo) {
+        const storageRef = storage.ref();
+        const databaseRef = database.ref("Pikkas");
+        const unique = String(uuid());
+        const name = unique.slice(0, unique.indexOf("-"));
+        databaseRef.push({ pikka: name, caption });
+        storageRef
+          .child(`/pikka/${name}`)
+          .put(photo)
+          .then(() => {
+            console.log(`Upload successfully`);
+            this.setState({ statusText: "OK" });
+          });
+      } else {
+        this.setState({ error: "A caption and file required" });
+      }
     } catch (e) {
-      console.error(`แอบเออเร่อนะจ๊ะ แต่เธอไม่รู้บ้างเลย`);
+      console.error(e.message);
+      this.setState({ error: e.message });
     }
   };
 
+  componentDidMount() {
+    (function() {
+      const uploader = document.getElementsByTagName("input");
+      uploader[1].onchange = function(event) {
+        console.log(`Change`);
+        let reader = new FileReader();
+        reader.onload = function() {
+          const output = document.getElementById("output");
+          output.src = reader.result;
+        };
+        reader.readAsDataURL(event.target.files[0]);
+      };
+    })();
+  }
+
   state = {
-	  statusText: ``
+    statusText: ``,
+    error: ``
   };
 
   render() {
     return (
-      <div className="measure center paper">
-	      {this.state.statusText === "OK" && <Redirect to="/"/>}
-        <center>
-          <p className="f4 lh-copy">
-            Pikka Node loves pictures, Let's Pikka<span className="text-success">
-              {" "}
-              Load!
-            </span>
-          </p>
-          <form
-            encType="multipart/form-data"
-            method="post"
-            onSubmit={this.handleFormSubmit}
-          >
-            <input
-              className="input-search"
-              name="caption"
-              placeholder="Add some captions"
-            />
-            <div className="pt2-ns">
-              <div className="wrapper">
-                <div className="file-upload">
-                  <p>Choose a file</p>
-                  <input
-                    name="photo"
-                    type="file"
-                    accept="image/jpeg, image/png"
-                  />
-                </div>
+      <div className="measure center paper" style={{ textAlign: "center" }}>
+        {this.state.statusText === "OK" && <Redirect to="/" />}
+        {this.state.error && (
+          <div className="alert alert-danger">{this.state.error}</div>
+        )}
+        <p className="f4 lh-copy">
+          Pikka Node loves pictures, Let's Pikka<span className="text-success">
+            {" "}
+            Load!
+          </span>
+        </p>
+        <form
+          encType="multipart/form-data"
+          method="post"
+          onSubmit={this.onFormSubmit}
+        >
+          <input
+            className="input-search"
+            name="caption"
+            placeholder="Add some captions"
+          />
+          <div className="pt2-ns">
+            <div className="wrapper">
+              <div className="file-upload">
+                <p>Choose a file</p>
+                <input
+                  name="photo"
+                  type="file"
+                  accept="image/jpeg, image/png"
+                  className="center"
+                />
               </div>
             </div>
-            <div className="pt2-ns">
-              <button className="submit" type="submit" value="Submit">
-                <i className="fas fa-upload fa-2x" />
-              </button>
-            </div>
-          </form>
-          <div className="pt2-ns">
-            <img id="output" alt="File output" />
           </div>
-        </center>
-        <script src="./scripts/uploadSuccess.js" />
+          <div className="pt2-ns">
+            <button className="submit" type="submit" value="Submit">
+              <i className="fas fa-upload fa-2x" />
+            </button>
+          </div>
+        </form>
+        <div className="pt2-ns">
+          <img id="output" alt="File output" />
+        </div>
       </div>
     );
   }
